2ba1c4ac85730c19a3551f7a56b52f37cf1c0bed
Thomas <Thomas@Thomass-MacBook-Pro-2.local>
2018-10-03 15:26:18 -0700

    Created Generator object and populated diceRollGenerator

--- js/randomGenerators.js
+++ js/randomGenerators.js
@@ -2,14 +2,20 @@
 // TODO: Use constructor notation to create an object
 // with properties and methods that can be shared by both
 // diceRollGenerator and eightBallGenerator.
-function Generator() {
-  // YOUR CODE HERE
-}
-
+function Generator(name, min, max, options) {
+this.name = name;
+this.max = max;
+this.min = min;
+this.options = options || [];
+this.randomResult = function() {
+  var x = math.random()* (max-min) + min;
+  return options[x]
+};
 // TODO: Initialise diceRollGenerator and eightBallGenerator
 // using the constructor notation and the Generator object
 // you just created
-const diceRollGenerator = null;
+const diceRollGenerator = new Generator("Dice roll", 1, 6, [1, 2, 3, 4, 5, 6
+]);
 const eightBallGenerator = null;
 
 function handleDiceRoll() {
@@ -24,7 +30,7 @@ function handleEightBallShake() {
   const resultElement = document.getElementById('eightBallResult');
 
   // TODO: call a method on eightBallGenerator to populate result with a random value
-  const result = "result";
+  const result = "result: ";
   resultElement.innerHTML = result;
 }
 
